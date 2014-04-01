758fe798e420dc53d99079d8516f4024e59da096
taoder <anthony.maia80@gmail.com>
2014-04-02 00:36:49 +0200

    amlioration design

--- main.py
+++ main.py
@@ -4,52 +4,48 @@ kivy.require('1.8.0')
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.graphics import *
-from kivy.properties import NumericProperty
+from kivy.properties import NumericProperty, ObjectProperty
 
 
-class TenPartOfBoard(Widget):
+class TenRound(Widget):
     pass
 
 
-class TenSquare(TenPartOfBoard):
+class TenSquare(Widget):
     pass
 
 
+class TenEmpty(Widget):
+    pass
 
 
-class TenGame(Widget):
+class TenBoard(Widget):
     cube_size = NumericProperty(10)
     cube_padding = NumericProperty(10)
 
     def __init__(self, **kwargs):
-        super(TenGame, self).__init__()
-
+        super(TenBoard, self).__init__()
         self.board = [[None for x in xrange(3)] for x in xrange(3)]
 
     def rebuild_background(self):
         self.canvas.before.clear()
         with self.canvas.before:
-            Color(0, 1, 0)
-            Rectangle(pos=self.pos, size=self.size)
-            Color(1, 0, 0)
             csize = self.cube_size, self.cube_size
             for ix, iy in self.iterate_pos():
-                Rectangle(pos=self.index_to_pos(ix, iy), size=csize)
+                TenEmpty(pos=self.index_to_pos(ix, iy), size=csize)
 
     def reposition(self, *args):
-        print("reposition")
         self.rebuild_background()
         # calculate the size of a number
         l = min(self.width, self.height)
-        padding = (l / 3.) / 12.
+        padding = (l / 3.) / 6.
         cube_size = (l - (padding * 4)) / 3.
         self.cube_size = cube_size
         self.cube_padding = padding
 
-        for ix, iy, board in self.iterate():
-            print("ici")
-            board.size = cube_size, cube_size
-            board.pos = self.index_to_pos(ix, iy)
+        for ix, iy, square in self.iterate():
+            square.size = cube_size, cube_size
+            square.pos = self.index_to_pos(ix, iy)
 
     def iterate(self):
         for ix, iy in self.iterate_pos():
@@ -70,14 +66,37 @@ class TenGame(Widget):
             (self.y + padding) + iy * (cube_size + padding)]
 
 
+class TenGame(Widget):
+    cube_size = NumericProperty(10)
+    cube_padding = NumericProperty(10)
+    board1 = ObjectProperty()
+    board2 = ObjectProperty()
+    board3 = ObjectProperty()
+    board4 = ObjectProperty()
+    board5 = ObjectProperty()
+    board6 = ObjectProperty()
+    board7 = ObjectProperty()
+    board8 = ObjectProperty()
+    board9 = ObjectProperty()
+
+    def __init__(self, **kwargs):
+        super(TenGame, self).__init__()
+
+        self.board = [
+            [self.board1, self.board2, self.board3],
+            [self.board4, self.board5, self.board6],
+            [self.board7, self.board8, self.board9]]
+
+    def on_touch_move(self, touch):
+        print("ici")
+        bsize = self.board1.size
+        bpos = self.board1.pos
+        self.board1 = TenSquare(pos=bpos, size=bsize)
+
+
 class TenApp(App):
     def build(self):
-        print("build TenApp")
-        game = TenGame()
-        game.reposition()
-        game.reposition()
-        return game
-
+        pass
 
 if __name__ == '__main__':
     TenApp().run()
