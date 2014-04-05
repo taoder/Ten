5ed7692a8ed763e4ff028a9ba87aaff174497f62
taoder <anthony.maia80@gmail.com>
2014-04-05 22:36:26 +0200

    amlioration graphisme

--- main.py
+++ main.py
@@ -11,9 +11,7 @@ import math
 
 class TenEmpty(Widget):
     index = NumericProperty(0)
-    #scale = NumericProperty(1)
-    cube_size = NumericProperty(10)
-    cube_padding = NumericProperty(10)
+    scale = NumericProperty(1)
 
 
 class TenRound(Widget):
@@ -31,13 +29,18 @@ class TenBoard(Widget):
 
     def __init__(self, **kwargs):
         super(TenBoard, self).__init__(**kwargs)
-        self.cells = [self.ids.cell1, self.ids.cell2, self.ids.cell3,
-                      self.ids.cell4, self.ids.cell5, self.ids.cell6,
-                      self.ids.cell7, self.ids.cell8, self.ids.cell9]
+        self.bind(pos=self.relayout, size=self.relayout)
+        self.cells = []
+        self.animations = []
+
+        for index in range(9):
+            cell = TenEmpty(index=index)
+            self.add_widget(cell)
+            self.cells.append(cell)
         self.relayout()
 
     def relayout(self, *args):
-        xmin, ymin, cs, _ = self.cube_info
+        xmin, ymin, cs, s, padding = self.cube_info
         x = xmin
         y = ymin
 
@@ -46,17 +49,19 @@ class TenBoard(Widget):
             cell.scale = cs
             if index % 3 == 2:
                 x = xmin
-                y += cs
+                y += cs + padding
             else:
-                x += cs
+                x += cs + padding
 
     @property
     def cube_info(self):
         s = min(self.width, self.height)
-        cs = s / 3
-        xmin = self.x + (self.width - s) / 2.
-        ymin = self.y + (self.height - s) / 2.
-        return xmin, ymin, cs, s
+
+        padding = (s / 3.) / 12
+        cs = (s - (padding * 4)) / 3.
+        xmin = self.x + padding
+        ymin = self.y + padding
+        return xmin, ymin, cs, s, padding
 
     """def collide_point(self, x, y):
         x = (x - self.x) / self.scale
@@ -92,7 +97,7 @@ class TenBoardLayout(Widget):
         self.relayout()
 
     def relayout(self, *args):
-        xmin, ymin, cs, _ = self.cube_info
+        xmin, ymin, cs, s, padding = self.cube_info
         x = xmin
         y = ymin
         cube_size = cs, cs
@@ -106,17 +111,68 @@ class TenBoardLayout(Widget):
                 board.size = cube_size
             if index % 3 == 2:
                 x = xmin
-                y += cs
+                y += cs + padding
             else:
-                x += cs
+                x += cs + padding
 
     @property
     def cube_info(self):
         s = min(self.width, self.height)
-        cs = s / 3
-        xmin = self.x + (self.width - s) / 2.
-        ymin = self.y + (self.height - s) / 2.
-        return xmin, ymin, cs, s
+
+        padding = (s / 3.) / 12
+        cs = (s - (padding * 4)) / 3.
+        xmin = self.x + (self.width - s) / 2. + padding
+        ymin = self.y + (self.height - s) / 2. + padding
+        return xmin, ymin, cs, s, padding
+
+    @property
+    def cube_info_focus(self):
+
+        index = self.board_focused_index
+
+        xmin, ymin, cs, side, padding = self.cube_info
+        t = math.ceil(((index / 3.) / -2.) * 2.) / 2.
+        x = xmin + ((index % 3) + (index % 3) / -2.) * (cs + padding)
+        y = ymin + (int(index / 3) + t) * (cs + padding)
+        cs_board_focused = side/1.5 - 2 * padding
+
+
+        xn = 1
+        yn = 1
+        size_index_x = 0
+        size_index_y = 0
+        if index == 0 or index == 1 or index == 2:
+            size_index_y = cs_board_focused
+        if index == 0 or index == 3 or index == 6:
+            size_index_x = cs_board_focused
+        if index == 2 or index == 5 or index == 8:
+            xn = -1
+        if index == 6 or index == 7 or index == 8:
+            yn = -1
+
+        _xmin = x + xn * ((index % 3) / 4. * cs_board_focused) + size_index_x
+        _ymin = y + yn * (math.floor(index / 3.) / 4. * cs_board_focused) + size_index_y
+        _side = side * 1 / 3
+        _padding = (_side / 3.) / 12
+        _cs = (_side - (_padding * 4)) / 3.
+        return x, y, cs_board_focused, _xmin, _ymin, _cs, _padding
+
+    @property
+    def cube_info_focus_milieu(self):
+
+
+        xmin, ymin, cs, side, padding = self.cube_info
+        t = math.ceil(((1 / 3.) / -2.) * 2.) / 2.
+        x = xmin + ((1 % 3) + (1 % 3) / -2.) * (cs + padding)
+        y = ymin + (int(1 / 3) + t) * (cs + padding)
+        cs_board_focused = side/1.5 - 2 * padding
+
+        _xmin = x + (1 / 4. * cs_board_focused)
+        _ymin = y + cs_board_focused
+        _side = side * 1 / 3
+        _padding = (_side / 3.) / 12
+        _cs = (_side - (_padding * 4)) / 3.
+        return x, y, cs_board_focused, _xmin, _ymin, _cs, _padding
 
     @property
     def board_focused(self):
@@ -129,33 +185,46 @@ class TenBoardLayout(Widget):
         board = self.board_focused
         self.remove_widget(board)
         self.add_widget(board)
-        xmin, ymin, cs, side = self.cube_info
-        t = math.ceil(((index / 3.) / -2.) * 2.) / 2.
-        x = xmin + ((index % 3) + (index % 3) / -2.) * cs
-        y = ymin + (int(index / 3) + t) * cs
+
+        if index == 4:
+            x, y, cs_board_focus, _xmin, _ymin, _cs, _padding = self.cube_info_focus_milieu
+        else:
+            x, y, cs_board_focus, _xmin, _ymin, _cs, _padding = self.cube_info_focus
+
         self.stop_animations()
-        self.animate(board, x=x, y=y, size=(side/1.5, side/1.5))
-        for _board in self.boards:
+        self.animate(board, x=x, y=y, size=(cs_board_focus, cs_board_focus))
+
+        _x = _xmin
+        _y = _ymin
+
+        for _index, _board in enumerate(self.boards):
             if board is _board:
+                if _index % 3 == 2:
+                    _x = _xmin
+                    _y += _cs + _padding
+                else:
+                    _x += _cs + _padding
                 continue
-            self.animate(_board)
+            self.animate(_board, x=_x, y=_y, size=(_cs,_cs))
+            if _index % 3 == 2:
+                _x = _xmin
+                _y += _cs + _padding
+            else:
+                _x += _cs + _padding
 
     def unfocus(self):
         index = self.board_focused_index
         if index == -1:
             return
         board = self.board_focused
-        xmin, ymin, cs, side = self.cube_info
-        x = xmin + (index % 3) * cs
-        y = ymin + int(index / 3) * cs
+        xmin, ymin, cs, side, padding = self.cube_info
+        x = xmin + (index % 3) * (cs + padding)
+        y = ymin + int(index / 3) * (cs + padding)
 
         self.stop_animations()
         self.animate(self.board_focused,
                 x=x, y=y, size=(cs, cs))
-        for _board in self.boards:
-            if board is _board:
-                continue
-            self.animate(_board)
+        self.relayout()
 
         self.board_focused_index = -1
 
@@ -165,8 +234,8 @@ class TenBoardLayout(Widget):
             anim.stop_all(widget)
 
     def animate(self, widget, **kwargs):
-        kwargs['d'] = .25
-        #kwargs['t'] = 'out_quart'
+        kwargs['d'] = .50
+        kwargs['t'] = 'out_quart'
         anim = Animation(**kwargs)
         anim.start(widget)
         self.animations.append((anim, widget))
